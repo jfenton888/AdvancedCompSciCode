b3906995d803e641bd41c6e878eae4850713e3a0
jfenton888 <mllax8@me.com>
2017-12-05 08:23:05 -0500

    Interactive Version

--- CS550 Winter/MA1 Git Quiz/hello.py
+++ CS550 Winter/MA1 Git Quiz/hello.py
@@ -1,15 +1,10 @@
 import sys
 
-names = ["John", "Henry", "Palmer", sys.argv[1]]
+names = ["John", "Henry", "Palmer", 0]
 
 
 for x in range(0, 3):
 	currentName = names[x]
 	print("Hello, "+ currentName + "!")
-#if names[3] == "Mrs. Healy" or names[3] == "Healy" or names[3] == "Meghan" or names[3] == "Ms. Hoke" or names[3] == "Mrs Healy":
-print(names[3].find("Healy"))
-
-if names[3] in ["Mrs. Healy", "Healy", "Meghan", "Ms. Hoke", "Mrs Healy"]:
-	print(names[3] + ", thank you for joining us!")
-else: 
-	print(names[3] , " what are you doing here? This isn't your table!")
+	if x == 2:
+		names[3]= input('And what is your name? ')
